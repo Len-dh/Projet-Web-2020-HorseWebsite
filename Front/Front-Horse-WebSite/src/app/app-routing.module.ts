4dc575c315dced7a17d288f8dba0bfa3328c0871
lulb8 <bonneville@et.esiea.fr>
2020-10-19 19:22:47 +0200

    Add registration form

--- Front/Front-Horse-WebSite/src/app/app-routing.module.ts
+++ Front/Front-Horse-WebSite/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [{ path: '', component: LoginComponent }];
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
