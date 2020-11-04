2f21a5f7339e3e91dd70b026d31d0adda9a30700
Lens <lenny.daho@gmail.com>
2020-11-04 12:19:00 +0100

    new Database with Admin, HI, Rider and WA + update on Front Web App

--- Back/Horse-Projet-5A-2020/src/main/java/com/example/HorseProjet5A2020/controller/RegistrationController.java
+++ Back/Horse-Projet-5A-2020/src/main/java/com/example/HorseProjet5A2020/controller/RegistrationController.java
@@ -1,14 +1,11 @@
 package com.example.HorseProjet5A2020.controller;
 
-import com.example.HorseProjet5A2020.model.User;
+import com.example.HorseProjet5A2020.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 import com.example.HorseProjet5A2020.service.RegistrationService;
-
-import javax.imageio.spi.RegisterableService;
 
 @RestController // return some response from the controller
 // Construction Form
@@ -31,6 +28,62 @@ public class RegistrationController {
         return userObj;
     }
 
+    @PostMapping("/registeradmin")
+    public Admin registerAdmin(@RequestBody Admin admin) throws Exception {
+        String tempEmailId = admin.getAdminEmailId();
+        if (tempEmailId != null && !"".equals(tempEmailId)){
+            Admin userobj = (Admin) service.fetchAdminByEmailId(tempEmailId);
+            if (userobj != null){
+                throw new Exception("admin with " + tempEmailId + " is already exist");
+            }
+        }
+        Admin userObj = null;
+        userObj = service.saveAdmin(admin);
+        return userObj;
+    }
+
+    @PostMapping("/registerHI")
+    public HorseInstructor registerHI(@RequestBody HorseInstructor hi) throws Exception {
+        String tempEmailId = hi.getHorseInstructorEmailId();
+        if (tempEmailId != null && !"".equals(tempEmailId)){
+            HorseInstructor userobj = service.fetchHIByEmailId(tempEmailId);
+            if (userobj != null){
+                throw new Exception("Horse Instructor with " + tempEmailId + " is already exist");
+            }
+        }
+        HorseInstructor userObj = null;
+        userObj = service.saveHI(hi);
+        return userObj;
+    }
+
+    @PostMapping("/registerRider")
+    public Rider registerRider(@RequestBody Rider rider) throws Exception {
+        String tempEmailId = rider.getRiderEmailId();
+        if (tempEmailId != null && !"".equals(tempEmailId)){
+            Rider userobj = service.fetchRiderByEmailId(tempEmailId);
+            if (userobj != null){
+                throw new Exception("Rider with " + tempEmailId + " is already exist");
+            }
+        }
+        Rider userObj = null;
+        userObj = service.saveRider(rider);
+        return userObj;
+    }
+
+    @PostMapping("/registerWA")
+    public WebsiteAdmin registerWA(@RequestBody WebsiteAdmin wa) throws Exception {
+        String tempEmailId =wa.getWebsiteAdminEmailId();
+        if (tempEmailId != null && !"".equals(tempEmailId)){
+            WebsiteAdmin userobj = service.fetchWAByEmailId(tempEmailId);
+            if (userobj != null){
+                throw new Exception("The Website Admin with " + tempEmailId + " is already exist");
+            }
+        }
+        WebsiteAdmin userObj = null;
+        userObj = service.saveWA(wa);
+        return userObj;
+    }
+
     @PostMapping("/login")
     public User loginUser (@RequestBody User user) throws Exception {
         User userObj = null;
@@ -43,4 +96,57 @@ public class RegistrationController {
         }
         return userObj;
     }
+
+    @PostMapping("/loginAdmin")
+    public Admin loginAdmin (@RequestBody Admin admin) throws Exception {
+        Admin userObj = null;
+        String tempEmailId = admin.getAdminEmailId(), tempPass = admin.getAdminPassword();
+        if(tempEmailId != null && tempPass != null){
+            userObj = service.fetchAdminByEmailIdAndPassword(tempEmailId, tempPass);
+        }
+        if (userObj == null){
+            throw new Exception("bad credentials");
+        }
+        return userObj;
+    }
+
+    @PostMapping("/loginHI")
+    public HorseInstructor loginHI (@RequestBody HorseInstructor hi) throws Exception {
+        HorseInstructor userObj = null;
+        String tempEmailId = hi.getHorseInstructorEmailId(), tempPass = hi.getHorseInstructorPassword();
+        if(tempEmailId != null && tempPass != null){
+            userObj = service.fetchHIByEmailIdAndPassword(tempEmailId, tempPass);
+        }
+        if (userObj == null){
+            throw new Exception("bad credentials");
+        }
+        return userObj;
+    }
+
+    @PostMapping("/loginRider")
+    public Rider loginRider (@RequestBody Rider rider) throws Exception {
+        Rider userObj = null;
+        String tempEmailId = rider.getRiderEmailId(), tempPass = rider.getRiderPassword();
+        if(tempEmailId != null && tempPass != null){
+            userObj = service.fetchRiderByEmailIdAndPassword(tempEmailId, tempPass);
+        }
+        if (userObj == null){
+            throw new Exception("bad credentials");
+        }
+        return userObj;
+    }
+
+    @PostMapping("/loginWA")
+    public WebsiteAdmin loginWA (@RequestBody WebsiteAdmin wa) throws Exception {
+        WebsiteAdmin userObj = null;
+        String tempEmailId = wa.getWebsiteAdminEmailId(), tempPass = wa.getWebsiteAdminPassword();
+        if(tempEmailId != null && tempPass != null){
+            userObj = service.fetchWAByEmailIdAndPassword(tempEmailId, tempPass);
+        }
+        if (userObj == null){
+            throw new Exception("bad credentials");
+        }
+        return userObj;
+    }
+
 }
--- Back/Horse-Projet-5A-2020/src/main/java/com/example/HorseProjet5A2020/model/User.java
+++ Back/Horse-Projet-5A-2020/src/main/java/com/example/HorseProjet5A2020/model/User.java
@@ -26,35 +26,12 @@ public class User {
         this.password = password;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getEmailId() {
         return emailId;
-    }
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
